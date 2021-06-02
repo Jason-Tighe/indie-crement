2dc87b6152394c6552e6e45ce53e61956f4f7995
slowdolphin <tighe59@gmail.com>
2021-06-02 19:56:22 -0400

    able to increment and decrement. unable to figure out the input box

--- script.js
+++ script.js
@@ -1,34 +1,53 @@
 //bring in the class/id's
 
 const container = document.querySelector(".container")
+
+
 const result = document.querySelector(".result")
 const minus = document.querySelector(".minus")
-const empty = document.querySelector(".input")
+const input = document.querySelector(".input")
 const plus = document.querySelector(".plus")
+
+
+const total = document.getElementById("bigPicture")
+const sub = document.getElementById("negative")
+const box = parseInt(document.getElementById("space").value,10)
+const add = document.getElementById("positive")
 
 
 //we're going to want the - to subtract and the + to add.
 // you want result + or - empty
 
 
-var result = 0;
-var empty = 0;
+
+
+
+
+
+var x = 0;
+
 
 function addition(){
-  result += empty
+  x++
+  input.innerHTML = x;
+  result.innerHTML = x
 }
 
 function subtract(){
-  resulty += empty
+  x--
+  input.innerHTML = x;
+  result.innerHTML = x
+  if(x < 0){
+    result.style.color = "red"
+  }
 }
 
 
-plus.addEventListener(){ 'click', (e)=>{
+
+plus.addEventListener( 'click', (e)=>{
   addition()
-  }
-}
+})
 
-minus.addEventListener(){ 'click', (e)=>{
+minus.addEventListener('click', (e)=>{
   subtract()
-  }
-}
+})
