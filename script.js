1abda998b9c533d0cc31276d498cad8e22da5300
slowdolphin <tighe59@gmail.com>
2021-06-02 22:08:24 -0400

    i did it. idk how, but i did.

--- script.js
+++ script.js
@@ -1,41 +1,36 @@
-//bring in the class/id's
 
 const container = document.querySelector(".container")
 
 
 const result = document.querySelector(".result")
 const minus = document.querySelector(".minus")
-const input = document.querySelector(".input")
 const plus = document.querySelector(".plus")
 
 
 const total = document.getElementById("bigPicture")
 const sub = document.getElementById("negative")
-const box = parseInt(document.getElementById("space").value,10)
 const add = document.getElementById("positive")
 
-const test = parseInt(document.getElementById("test").value,10)
-//we're going to want the - to subtract and the + to add.
-// you want result + or - empty
-//i'm having an issue with the last part.
+var test = document.getElementById("test")
+var val = parseInt(document.querySelector('input.test').value, 10);
 
 
 
 var x = 0;
 
 
+
 function addition(){
-  x++
-  input.innerHTML = x;
+  x+= parseInt(test.value)
   result.innerHTML = x
   if(x >= 0){
     result.style.color = "white"
   }
 }
 
+
 function subtract(){
-  x--
-  input.innerHTML = x;
+  x-=parseInt(test.value)
   result.innerHTML = x
   if(x < 0){
     result.style.color = "red"
@@ -70,5 +65,6 @@ plus.addEventListener( 'click', (e)=>{
 })
 
 minus.addEventListener('click', (e)=>{
+
   subtract()
 })
